c55cdf66871f45e8ad8a952a26245229fa1d44ed
Helena <higuima13@gmail.com>
2021-11-19 01:07:23 -0300

    Phrase component: initial draft

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,6 +3,7 @@ import AboutMe from './AboutMe';
 import './App.css';
 import BackgroundCircle from './BackgroundCircles';
 import Menu from './Menu';
+import Phrase from './Phrase';
 import Work from './Work';
 export default class App extends React.Component{
   render() {
@@ -10,6 +11,7 @@ export default class App extends React.Component{
       <div className="App">
         {/* <BackgroundCircle/> */}
         <Menu/>
+        <Phrase/>
         <AboutMe/>
         <Work/>
       </div>
---
+++ src/components/Phrase.tsx
@@ -0,0 +1,14 @@
+
+import { Component } from "react"
+import './Phrase.css'
+
+export default class Menu extends Component {
+    render(){
+        return (
+            <div className="Phrase-container">
+                <h1>Creativity is intelligence having fun!</h1>
+                <p>Albert Einstein</p>
+            </div>
+        )
+    }
+}
